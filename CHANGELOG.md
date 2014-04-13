86aa2717e140634daa381bd8e1e13fe5bb24999a
Josef Florian Glatz <jousch@gmail.com>
2014-04-13 11:44:35 +0200

    [TASK] Initial release of grapicsmagick cookbook
    * adds CHANGELOG
    * adds metadata file
    * adds default recipe
    * adds README

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+graphicsmagick CHANGELOG
+=====================
+
+This file is used to list changes made in each version of the graphicsmagick cookbook.
+
+1.0.0
+-----
+- [Josef Florian Glatz] - Initial release of graphicsmagick
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'graphicsmagick'
+maintainer       'Josef Florian Glatz'
+maintainer_email 'jousch@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures graphicsmagick'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
